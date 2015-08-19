ddc8a2ac864996d4e5c4fb0bd70ca7deaaf872cb
Amit <amitshob@gmail.com>
2015-08-19 10:05:17 -0700

    edit wordcount(removed self) edit gemfile

--- lib/wordcount.rb
+++ lib/wordcount.rb
@@ -2,19 +2,18 @@ class String
   define_method(:wordcount) do |word_input|
 
     #Make all the sentence components lowercase
-    string_input = self.downcase()
+    string_input = downcase()
 
     #Split sentence in components
     string_input = string_input.split()
 
-
     #Make the inpout word lowercase
-    word_input=self.downcase()
+    word_input = word_input.downcase()
 
     #count the occurence of the word in the sentence
-    word_freq=0
+    word_freq = 0
     string_input.each() do |word|
-      if word.eql?(word_input) == true
+      if word.eql?(word_input)
          word_freq = word_freq.+(1)
       end
     end
